56e31309273267ef7fa6acdfc2fbd2923af0d611
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2021-09-29 21:04:48 +0900

    add code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,18 @@
 import Head from "next/head";
 import { Footer } from "src/Components/Footer";
-import { Header } from "src/Components/Header";
+// import { Header } from "src/Components/Header";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+		<div className="flex flex-col items-center justify-center min-h-screen">
 			<Head>
 				<title>Create Next App</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Header />
 			<main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
 				<div>
-					<div>
-						<h1>Profile</h1>
-						<div></div>
-						<div></div>
-					</div>
+					<h1>About</h1>
+					<div></div>
 				</div>
 			</main>
 			<Footer />
