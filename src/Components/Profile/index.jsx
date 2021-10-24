9919531d2905a787048bb778b5242fbee032d801
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2021-10-24 15:28:38 +0900

    add code

--- src/Components/Profile/index.jsx
+++ src/Components/Profile/index.jsx
@@ -1,10 +1,24 @@
+import { Image } from "@chakra-ui/react";
+
 export function Profile() {
 	return (
 		<div>
-			<h2 className="font-serif font-bold text-4xl">Profile</h2>
-			<div>
-				<div className=""></div>
-				<div></div>
+			<div className="text-left text-4xl font-serif font-bold">
+				<p>Hi!!!!</p>
+				<p>I'm WebEngineer!</p>
+			</div>
+			<div className="flex flex-row space-x-6 pt-6 justify-center items-center">
+				<Image
+					borderRadius="full"
+					boxSize="100px"
+					src="Nanase.jpeg"
+					alt="Segun Adebayo"
+				/>
+				<div className="text-left font-bold font-serif text-xl">
+					<p>My Name is</p>
+					<p>Naoki.</p>
+					<p></p>
+				</div>
 			</div>
 		</div>
 	);
